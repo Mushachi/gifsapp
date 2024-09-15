8d2f6118d17b33dc2b3d8bc8d4a064dec4516b11
Julio Barriga <mushachi@gmail>
2024-09-15 12:59:05 -0600

    ViewChild - Referencia al HTML

--- src/app/gifs/components/search-box/search-box.component.ts
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'gifs-search-box',
@@ -7,9 +7,21 @@ import { Component } from '@angular/core';
     <input type="text"
       class="form-control"
       placeholder="Buscar gifs..."
+      (keyup.enter)="serchTag()"
+      #txtTagInput
     >
   `
 })
 export class SearchBoxComponent {
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>
+
+  serchTag() {
+
+    const newTag = this.tagInput.nativeElement.value
+
+    console.log({ newTag });
+  }
 
 }
