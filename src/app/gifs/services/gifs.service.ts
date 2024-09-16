3a93db7998e8104c4ea372ebd6617f3db4c59808
Julio Barriga <mushachi@gmail>
2024-09-15 19:24:40 -0600

    Realizar una petición HTTP

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+// const TENOR_API_KEY = 'AIzaSyD6VlY2SH8kOJEFwhnlkrn44WIg0XKnHk0';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +9,14 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  // Desde API Tenor https://developers.google.com/tenor/guides/quickstart?hl=es-419
+  // private apiKey: string = 'AIzaSyD6VlY2SH8kOJEFwhnlkrn44WIg0XKnHk0';
 
-  constructor() { }
+  // Desde API Giphy
+  private apiKey: string = 'i3CcKRdfhUyt3oYU20Znn00m7PCsewDW';
+  private serviceUrl: string = 'http://api.giphy.com/v1/gifs';
+
+  constructor( private http: HttpClient ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -24,13 +33,50 @@ export class GifsService {
     this._tagsHistory = this.tagsHistory.splice(0,10);
   }
 
-  searchTag( tag: string ):void {
+  searchTag2( tag: string ):void {
     if ( tag.length === 0 ) return;
 
     // this._tagsHistory.unshift( tag );
     this.organizaHistorial(tag);
 
-    console.log(this.tagsHistory);
+    // console.log(this.tagsHistory);
   }
+
+  async searchTag3( tag: string ):Promise<void> {
+    if ( tag.length === 0 ) return;
+    this.organizaHistorial(tag);
+
+
+    fetch('http://api.giphy.com/v1/gifs/search?api_key=i3CcKRdfhUyt3oYU20Znn00m7PCsewDW&q=goku&limit=10')
+      .then( resp => resp.json() )
+      .then( data => console.log(data) );
+  }
+
+  async searchTag4( tag: string ):Promise<void> {
+    if ( tag.length === 0 ) return;
+    this.organizaHistorial(tag);
+
+
+    const resp = await fetch('http://api.giphy.com/v1/gifs/search?api_key=i3CcKRdfhUyt3oYU20Znn00m7PCsewDW&q=goku&limit=10');
+    const data = await resp.json();
+    console.log(data);
+  }
+
+  async searchTag( tag: string ):Promise<void> {
+    if ( tag.length === 0 ) return;
+    this.organizaHistorial(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey )
+      .set('limit', '10' )
+      .set('q', tag )
+
+    this.http.get(`${ this.serviceUrl }/search`, { params: params })
+      .subscribe( resp=> {
+        console.log(resp);
+      });
+  }
+
+
 
 }
